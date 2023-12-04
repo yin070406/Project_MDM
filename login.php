14781e45a3becb3e5f3508a7448092651d080601
yin070406 <64211270+yin070406@users.noreply.github.com>
2023-12-04 22:11:24 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php 
 session_start(); 
-include "db_conn.php";
+include "dbConnect.php";
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
 
@@ -11,26 +11,25 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 	   return $data;
 	}
 
-	$uname = validate($_POST['username']);
-	$pass = validate($_POST['password']);
+	$username = validate($_POST['username']);
+	$password = validate($_POST['password']);
 
-	if (empty($uname)) {
+	if (empty($username)) {
 		header("Location: index.php?error=Username is required");
 	    exit();
-	}else if(empty($pass)){
+	}else if(empty($password)){
         header("Location: index.php?error=Password is required");
 	    exit();
 	}else{
-		$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+		$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
-            if ($row['user_name'] === $uname && $row['password'] === $pass) {
-            	$_SESSION['user_name'] = $row['user_name'];
+            if ($row['username'] === $username && $row['password'] === $password) {
+            	$_SESSION['username'] = $row['username'];
             	$_SESSION['name'] = $row['name'];
-            	$_SESSION['id'] = $row['id'];
             	header("Location: home.php");
 		        exit();
             }else{
